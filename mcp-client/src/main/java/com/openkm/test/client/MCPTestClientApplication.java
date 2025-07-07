afd197be7731046a29914fb796b97c3e2bcc5836
Josep Llort Tella <jllort@openkm.com>
2025-07-07 16:56:26 +0200

    initial commit

---
+++ mcp-client/src/main/java/com/openkm/test/client/MCPTestClientApplication.java
@@ -0,0 +1,11 @@
+package com.openkm.test.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MCPTestClientApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MCPTestClientApplication.class, args);
+	}
+}
